6d5bcafc0da425f8c1a19f8ea7734e1d2d59d165
Feather <lisafeather@gmail.com>
2014-05-07 09:51:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewExquisiteCorpse::Application.config.secret_key_base = '165342456ddf65906bbdec2ccf50c3dba8c802f11545c5aa832da3c2f5449227bc54aa0d7ee1b71e481dca408f8855fa9f61980611cc08bc6da3e3ccbcae61ad'
