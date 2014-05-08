ccdb16444c12b0ea571d4159bda5af7f4d44c5f7
Feather <lisafeather@gmail.com>
2014-05-08 12:41:36 -0700

    first failing test

--- spec/views/corpse/index.html.erb_spec.rb
+++ spec/views/corpse/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe "corpse/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "displays hello world" do
+    render
+    rendered.should include("hello world")
+  end
 end
